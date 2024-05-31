5a9404d8d47d1aeb92952ba4bee205895f62ed00
Jude010 <lysaghtjude@gmail.com>
2024-05-31 15:11:06 +0100

    fixes

--- TGDsite/db/connect.py
+++ TGDsite/db/connect.py
@@ -5,7 +5,7 @@ def get_db_conn():
     #connect to postgres db using environment variables
     conn = psycopg2.connect(
         host="localhost",
-        database="siteDB",
+        database="sitedb",
         #user=os.environ["DB_USERNAME"],
         #password=os.environ['DB_PASSWORD']
         user='flask',
