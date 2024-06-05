992d7fb3319cf6052676eabc8b88f632bbc48a4f
Jude010 <lysaghtjude@gmail.com>
2024-06-05 11:37:28 +0100

    read text work

--- TGDsite/db/readText.py
+++ TGDsite/db/readText.py
@@ -1,4 +1,11 @@
 import xml.etree.ElementTree as ET
 
-def readText() :
+# function to read text from guidetext.xml using 
+def readText(g_names):
+    tree = ET.parse('~/TGDsite/resources/guideText.xml')
+    root = tree.getroot()
+
+    # for each name passed find the text associated and append it to a dictioany
+    for name in g_names :
+        g_text = root.find(".//guide[@id='" + name + "']").text
     
