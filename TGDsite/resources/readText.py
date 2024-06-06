32540b9e7d5f9fa4df50aaffe35b4349ceec5428
Jude010 <lysaghtjude@gmail.com>
2024-06-06 10:05:19 +0100

    paragraph

--- TGDsite/resources/readText.py
+++ TGDsite/resources/readText.py
@@ -11,6 +11,7 @@ def readText(g_names):
     # for each name passed find the text associated and append it to a dictionary
     for name in g_names :
         g_text = root.find(".//guide[@name='" + name[0] + "']").text
+        g_text = g_text.split("\n")
         out[name[0]] = g_text 
     
     return out
