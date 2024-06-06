5898e24177ee7c1d8bbff7a29c28ec935d64e8d5
Jude010 <lysaghtjude@gmail.com>
2024-06-06 15:35:47 +0100

    basic questions progress

--- TGDsite/questions.py
+++ TGDsite/questions.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template , Blueprint
+from flask import Flask , render_template , Blueprint , request
 from TGDsite.db import connect
 from TGDsite.resources import readText
 
@@ -13,13 +13,8 @@ def index():
     guides = cur.fetchall()
     return render_template('questions.html', guides=guides)
     
-@bp.route('/guides')
+@bp.route('/guides', methods=['GET'])
 def guides():
-    conn = connect.get_db_conn()
-    cur = conn.cursor()
-    cur.execute('SELECT guide_name FROM guide;')
-    guides = cur.fetchall()
-    cur.close()
-    conn.close()
+    guides = request.form['']
     text = readText.readText(guides)
     return render_template('guides.html' )
