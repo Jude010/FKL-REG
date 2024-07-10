6556ec718b86796d39b3f857971cbaa3f24148e3
jude010 <lysaghtjude@gmail.com>
2024-07-10 14:40:56 +0100

    save function

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ Vagrant + Virtualbox - VM hosting \
 jinja/html/css - web design
 
 
+project shape in session
+
+project{name, privacy, floors, stair_num, stairs{stair(i){name , inside ,rise ,part_m}}}
--- TGDsite/db/save.py
+++ TGDsite/db/save.py
@@ -1,8 +1,23 @@
 import connect
 
-def save_to_db(project):
+def save_to_db(project , user):
     #save project to db
     conn = connect.get_db_conn()
     cur = conn.cursor()
 
-    cur.execute("INSERT INTO project (name , privacy , floors)")
+    # insert project details into DB and recieve new project ID
+    cur.execute("INSERT INTO project (name , privacy , floors) values ('" + project['name'] + "' ,'" + project['privacy'] + "' ,'"  + project['floors'] + "') RETURNING proj_id;")
+    pid = cur.fetchone[0]
+
+    # fetch current user_id from users DB and insert it and new project id into saves
+    cur.execute("SELECT user_id FROM users WHERE username LIKE '" + user + "';")
+    cur.execute("INSERT INTO saves (user_id , proj_id)")
+
+    # for each stair in project inser data into db and connect the project id and stair id in DB
+    for i in range(project['stair_num']) :
+        stair = project['stairs'][str(i)]
+        cur.execute("INSERT INTO stairs (name , rise , part_m inside) values ('" + stair['name'] + "' ,'" + stair['rise']  + "' ,'" + stair['part_m'] + "' ,'" + stair['inside'] + "') RETURNING stair_id;")
+        sid = cur.fetchone[0]
+        cur.execute("INSERT INTO proj_stair (stair_id , proj_id) values ('" + sid + "' ,'" + pid + "')")
+
+    
