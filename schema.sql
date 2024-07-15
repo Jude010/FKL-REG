5c81e2f01ed3205be34948efa6c5c79e0edece26
jude010 <lysaghtjude@gmail.com>
2024-07-15 15:39:20 +0100

    test

--- schema.sql
+++ schema.sql
@@ -60,11 +60,11 @@ CREATE TABLE proj_stair (
 );
 
 --add test user
-INSERT INTO users (username, password) VALUES ('test' , 'open');
+INSERT INTO users (username, password) VALUES 'test' , 'open';
 
 --add test project
 INSERT INTO project (pname , floors , privacy) SELECT ('test', '1' , 'private');
-INSERT INTO saves (user_id , proj_id) SELECT user_id FROM project p  WHERE (u.username LIKE 'test') SELECT proj_id FROM project p  project p WHERE (p.name LIKE 'test');
+INSERT INTO saves (user_id , proj_id) VALUES (SELECT user_id FROM project p  WHERE (u.username LIKE 'test')) ,  (SELECT proj_id FROM project p  project p WHERE (p.name LIKE 'test'));
 
 -- insert 
 INSERT INTO guide (guide_name, diagram) VALUES
