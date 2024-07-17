28a8dab8a81ef7df971d7cfff600d222afbf84e6
jude010 <lysaghtjude@gmail.com>
2024-07-17 17:16:24 +0100

    test

--- TGDsite/saves.py
+++ TGDsite/saves.py
@@ -1,4 +1,4 @@
-from flask import Blueprint ,render_template , session
+from flask import Blueprint ,render_template , session , request
 from TGDsite.db import connect 
 
 bp = Blueprint('saves' , __name__ , url_prefix='/saves')
@@ -10,7 +10,7 @@ def load_saves():
     cur = conn.cursor()
 
     # fetch all project names associated with current user
-    cur.execute("SELECT pname FROM project p INNER JOIN users u ON p.user_id = u.user_id WHERE u.username = '" + user + "';")
+    cur.execute("SELECT pname , proj_id FROM project p INNER JOIN users u ON p.user_id = u.user_id WHERE u.username = '" + user + "';")
     #cur.execute("SELECT pname FROM project;")
     names = cur.fetchall()
     conn.close()
@@ -20,5 +20,16 @@ def load_saves():
 
 @bp.route('/display_save', methods=['POST'])
 def display_save():
-    
-    return
+    results = request.form
+    user = session['user']
+    conn = connect.get_db_conn()
+    cur = conn.cursor()
+
+    #fetch all project details using project id from POST
+    cur.execute("SELECT pname ,floors ,privacy FROM project p WHERE proj_id = '" + request['id'] + "';" )
+    proj = cur.fetchall()
+
+    cur.execute("SELECT stair_id WHERE proj_id = '" + request['id'] + "';" )
+
+
+    return render_template('load_project.html.jinja')
