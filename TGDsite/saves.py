4d59d13ebba4ef85d1b727a6d8b17998eed57644
jude010 <lysaghtjude@gmail.com>
2024-07-17 15:44:02 +0100

    move towards load saves

--- TGDsite/saves.py
+++ TGDsite/saves.py
@@ -5,13 +5,13 @@ bp = Blueprint('saves' , __name__ , url_prefix='/saves')
 
 @bp.route('/load_saves')
 def load_saves():
-    #user = session['user']
+    user = session['user']
     conn = connect.get_db_conn()
     cur = conn.cursor()
 
     # fetch all project names associated with current user
-    #cur.execute("SELECT pname FROM project p INNER JOIN saves s ON p.proj_id = s.proj_id INNER JOIN users u ON s.user_id = u.user_id WHERE username = '" + user + "';")
-    cur.execute("SELECT pname FROM project;")
+    cur.execute("SELECT pname FROM project p INNER JOIN users U ON p.user_id = u.user_id INNER JOIN users u ON p.user_id = u.user_id WHERE username = '" + user + "';")
+    #cur.execute("SELECT pname FROM project;")
     names = cur.fetchall()
     conn.close()
 
@@ -21,4 +21,4 @@ def load_saves():
 @bp.route('/display_save', methods=['POST'])
 def display_save():
     
-    return None
+    return
