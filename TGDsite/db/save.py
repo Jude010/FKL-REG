9c87c3a284b7bca7f3648211095a454ebef52f0f
jude010 <lysaghtjude@gmail.com>
2024-07-19 15:38:41 +0100

    test

--- TGDsite/db/save.py
+++ TGDsite/db/save.py
@@ -3,9 +3,10 @@ from . import connect
 def save_to_db(project , user , signature , date):
     #save project to db
     conn = connect.get_db_conn()
+
     cur = conn.cursor()
     cur.execute("SELECT * FROM users;")
-    print(cur.fetchall)
+    print(cur.fetchall())
     # insert project details into DB and recieve new project ID
     cur.execute("INSERT INTO project (pname , privacy , floors) SELECT '" + project['name'] + "' ,'" + project['privacy'] + "' ,'"  + project['floors'] + "' user_id FROM users  WHERE username = '" + user + "' RETURNING proj_id ;")
     pid = cur.fetchone()[0]
