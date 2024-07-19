44e30bf8a25d0544ccb69e3f88db2a8a05e72e23
jude010 <lysaghtjude@gmail.com>
2024-07-19 15:04:05 +0100

    str(bool)

--- TGDsite/saves.py
+++ TGDsite/saves.py
@@ -61,7 +61,6 @@ def display_save():
 def save_project():
     results = request.form
     project = session['project']
-    print(project)
     save.save_to_db(project ,session['user'],results['signature'],results['date'])
 
     return render_template('load_project.html.jinja' , project = session['project'])
