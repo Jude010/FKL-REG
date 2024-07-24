085cdd67615388bbaf73defef3ab4342565615a9
jude010 <lysaghtjude@gmail.com>
2024-07-24 16:09:28 +0100

    ramp object done

--- TGDsite/resources/project_parts.py
+++ TGDsite/resources/project_parts.py
@@ -1,15 +1,19 @@
 
 
 class project:
-    def __init__(self, name, privacy , stair_no=0, floors=1):
+    def __init__(self, name, privacy , stair_no=0, floors=1 , ramp_no=0):
         self.stairs = []
+        self.ramps = []
         self.privacy = privacy
         self.name = name
         self.floors = floors
 
-        # initalise a number of stairs equal to number and names them stair1 to stair(number)
+        # initalise a number of stairs and ramps equal to number and names them stair/ramp_1 to stair/ramp_(number)
         for i in range(int(stair_no)):
             self.stairs.append(stair("stair_" + str(i)))
+
+        for i in range(int(ramp_no)):
+            self.ramps.append(ramp("ramp_" + str(i)))
 
 
     def stair_add(self, stair):
@@ -51,5 +55,24 @@ class stair:
                 "inside": self.inside,
                 "rise": self.rise,
                 "part_m": self.part_m}
+    
+
+class ramp:
+    def __init__(self , name) -> None:
+        self.inside  = ''
+        self.name = name
+        self.rise = 0
+        self.part_m = False
+        self.width = 0
+
+    # conver to dict
+    def serialize(self):
+        return {"name": self.name,
+                "inside": self.inside,
+                "rise": self.rise,
+                "part_m": self.part_m,
+                "width": self.width}
+
+
 
     
