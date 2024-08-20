d0ed4ee0a29d725909fc952fc114f4455425445f
jude010 <lysaghtjude@gmail.com>
2024-08-20 11:53:08 +0100

    test

--- TGDsite/questions.py
+++ TGDsite/questions.py
@@ -39,7 +39,7 @@ def stair_questions_1():
     if project['stair_num'] > 0:
         return render_template('stair_questons.html.jinja' , project = project)
     else :
-        return redirect(url_for('questions.ramp_questions'))
+        return redirect(url_for('.ramp_questions' , code=307))
 
 @bp.route('/ramps', methods=['POST'])
 def ramp_questions():
